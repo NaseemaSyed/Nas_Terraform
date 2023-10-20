5bd4ceebbc904e72d9b7710bdee2aef1dd3e3d8e
shaikna <naseema.shaik@mercer.com>
2023-10-20 14:24:58 +1100

    AzCICDpipe

--- AnsibleEnv/Ansibleenv.tf
+++ AnsibleEnv/Ansibleenv.tf
@@ -1,35 +1,29 @@
-terraform {
-  required_providers {
-
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">3.0.0"
-    }
-  }
-}
-
 provider "azurerm" {
   subscription_id            = var.subscription_id
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
+provider "random" {
+  # Configuration options
+}
+
 # Create a resource group
 resource "azurerm_resource_group" "nas" {
-  name     = "nas-rgansibleenv"
+  name     = "${var.prefix}-rgansibleenv"
   location = var.location
 }
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "nas" {
-  name                = "nas-network1"
+  name                = "${var.prefix}-network1"
   resource_group_name = azurerm_resource_group.nas.name
   location            = azurerm_resource_group.nas.location
   address_space       = [var.address_space]
 }
 
 resource "azurerm_network_security_group" "nas" {
-  name                = "nasSecurityGroup1"
+  name                = "${var.prefix}-SecurityGroup1"
   location            = azurerm_resource_group.nas.location
   resource_group_name = azurerm_resource_group.nas.name
 
@@ -49,7 +43,7 @@ resource "azurerm_network_security_group" "nas" {
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "*"
+    protocol                   = "RDP"
     source_port_range          = "*"
     destination_port_range     = "3389"
     source_address_prefix      = "*"
@@ -58,6 +52,7 @@ resource "azurerm_network_security_group" "nas" {
 
   tags = {
     environment = "Dev"
+    source = "terraform"
   }
 }
 resource "azurerm_subnet" "nas" {
@@ -66,9 +61,23 @@ resource "azurerm_subnet" "nas" {
   virtual_network_name = azurerm_virtual_network.nas.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+# Create public IPs
+resource "azurerm_public_ip" "linux_public_ip" {
+  name                = "LinuxPublicIP"
+  location            = azurerm_resource_group.nas.location
+  resource_group_name = azurerm_resource_group.nas.name
+  allocation_method   = "Dynamic"
+}
+# Create public IPs
+resource "azurerm_public_ip" "win_public_ip" {
+  name                = "WinPublicIP"
+  location            = azurerm_resource_group.nas.location
+  resource_group_name = azurerm_resource_group.nas.name
+  allocation_method   = "Dynamic"
+}
 
 resource "azurerm_network_interface" "nas" {
-  name                = "nas-nic"
+  name                = "${var.prefix}-nic_l"
   location            = azurerm_resource_group.nas.location
   resource_group_name = azurerm_resource_group.nas.name
 
@@ -76,11 +85,12 @@ resource "azurerm_network_interface" "nas" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.nas.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.linux_public_ip.id
   }
 }
 
 resource "azurerm_network_interface" "nas2" {
-  name                = "nas-nic2"
+  name                = "${var.prefix}-nic_w"
   location            = azurerm_resource_group.nas.location
   resource_group_name = azurerm_resource_group.nas.name
 
@@ -88,13 +98,15 @@ resource "azurerm_network_interface" "nas2" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.nas.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.win_public_ip.id
   }
 }
+# Create Linux VM
 resource "azurerm_linux_virtual_machine" "nas" {
-  name                = "nas-ubuntu"
+  name                = "${var.prefix}-ubuntu"
   resource_group_name = azurerm_resource_group.nas.name
   location            = azurerm_resource_group.nas.location
-  size                = "Standard_B1ls"
+  size                = "Standard_B2s"
   admin_username      = var.admin_username
   network_interface_ids = [
     azurerm_network_interface.nas.id,
@@ -117,14 +129,21 @@ resource "azurerm_linux_virtual_machine" "nas" {
     version   = "latest"
   }
 }
-
+#Create Random password for admin account
+resource "random_password" "random_pass" {
+  length           = 9
+  special          = true
+  upper            = true
+  override_special = "/@£$"
+}
+#Create Windows VM
 resource "azurerm_windows_virtual_machine" "nas" {
-  name                = "nas-windows"
+  name                = "${var.prefix}-windows"
   resource_group_name = azurerm_resource_group.nas.name
   location            = azurerm_resource_group.nas.location
-  size                = "Standard_B1ls"
+  size                = "Standard_B2s"
   admin_username      = var.admin_username
-  admin_password      = var.password
+  admin_password      = random_password.random_pass
   network_interface_ids = [
     azurerm_network_interface.nas2.id,
   ]
