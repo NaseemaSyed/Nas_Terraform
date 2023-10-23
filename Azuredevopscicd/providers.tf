bea1e976a174fd69b5b2b15e0d6b0a0cb45904f6
NaseemaSyed <87567525+NaseemaSyed@users.noreply.github.com>
2023-10-23 14:20:40 +1100

    providers.tf

--- Azuredevopscicd/providers.tf
+++ Azuredevopscicd/providers.tf
@@ -18,10 +18,10 @@ terraform {
 
   }
   backend "remote" {
-    organization = "ned-in-the-cloud"
+    organization = "naseema510"
 
     workspaces {
-      name = "terraform-tuesday-ado-setup"
+      name = "terraform"
     }
   }
 }
